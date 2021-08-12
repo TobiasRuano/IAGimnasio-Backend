1c30633fce0a612f62cc487acf75be0f4e32e7b2
Tobias Ruano <ruano.t10@gmail.com>
2021-08-12 18:58:12 -0300

    Estructura basica del proyecto

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({
+    extended: false
+}));
+
+module.exports = app;
