b3b35d5954a350388c5f452c5463225832684397
Tobias Ruano <ruano.t10@gmail.com>
2021-09-01 16:17:34 -0300

    implementacion de la funcion con errores

--- controllers/finance.controller.js
+++ controllers/finance.controller.js
@@ -1,6 +1,7 @@
 const models = require('../models');
 const moment = require('moment');
 const sequelize = require('../models/index.js');
+const { Op } = require("sequelize");
 const fs = require('fs');
 var path = require('path');
 const { start } = require('repl');
@@ -44,7 +45,7 @@ function createSubscription(req, res) {
     });
 }
 
-// TODO: implementar pago de la subscripcion
+// TODO: Transaccions pago de la subscripcion
 function setSubscription(req, res) {
     models.User.findOne({where:{dni:req.body.dni}}).then(user => {
         if(user) {
@@ -131,35 +132,19 @@ async function paySubscription(metodo, res) {
     }
 }
 
+// periodo fecha de inicio y fecha de fin id
 function calculatePayroll(req, res) {
-    models.employee.findOne({where:{dni:req.body.dni}}).then(employee => {
+    models.Employee.findOne({where:{id:req.body.id}}).then(employee => {
         if(employee) {
-            const sueldoTotal = employee.hoursWorked * employee.salaryPerHour;
-            const jubilacion = sueldoTotal * 0.11;
-            const obraSocial = sueldoTotal * 0.03;
-            const pami = sueldoTotal * 0.03;
-
-            const salario = {
-                date: getCurrentDate(),
-                period: req.body.periodo,
-                employeeID: employee.id,
-                jubilacion: jubilacion,
-                obraSocial: obraSocial,
-                pami: pami,
-                total: (sueldoTotal - jubilacion - obraSocial - pami)
+            if(employee.type == 1) {
+                getHoursWorked(employee.id, req.body.fechaInicio, req.body.fechaFin).then(async hoursWorked => {
+                    console.log("El valor de hoursWorked es: ")
+                    console.log(hoursWorked);
+                    paySalary(req, hoursWorked * employee.salaryPerHour, employee, res);
+                });
+            } else {
+                paySalary(req, employee.hoursWorked * employee.salaryPerHour, employee, res);
             }
-
-            models.Wages.create(salario).then(result => {
-                res.status(200).json({
-                    message: "Sueldo liquidado para el trabador!",
-                    sueldo: result
-                });
-            }).catch(error => {
-                res.status(500).json({
-                    message: "Hubo un error al liquidar el sueldo.",
-                    error: error
-                });
-            });
         } else {
             res.status(404).json({
                 message: "No existe un empleado para ese ID."
@@ -170,6 +155,49 @@ function calculatePayroll(req, res) {
             message: "Something went wrong!",
             error: error
         });
+    });
+}
+
+function paySalary(req, sueldoTotal, employee, res) {
+    const jubilacion = sueldoTotal * 0.11;
+    const obraSocial = sueldoTotal * 0.03;
+    const pami = sueldoTotal * 0.03;
+
+    const salario = {
+        date: getCurrentDate(),
+        period: req.body.periodo,
+        employeeID: employee.id,
+        jubilacion: jubilacion,
+        obraSocial: obraSocial,
+        pami: pami,
+        total: (sueldoTotal - jubilacion - obraSocial - pami)
+    }      
+
+    res.status(200).json({
+        message: "Sueldo liquidado para el trabador!",
+        sueldo: salario
+    });
+    /*
+    models.Wages.create(salario).then(result => {
+        res.status(200).json({
+            message: "Sueldo liquidado para el trabador!",
+            sueldo: result
+        });
+    }).catch(error => {
+        res.status(500).json({
+            message: "Hubo un error al liquidar el sueldo.",
+            error: error
+        });
+    });*/
+}
+
+function getHoursWorked(trainnerID, startDate, endDate) {
+    const start = moment(startDate, "YYYY-MM-DD hh:mm:ss");
+    const end = moment(endDate, "YYYY-MM-DD hh:mm:ss");
+
+    models.Appointment.findAll({where:{trainnerID:trainnerID, dateTimeStart: { [Op.gt]: start , [Op.lt]: end}}}).then(result => {
+        console.log(result.length)
+        return result.length
     });
 }
 
