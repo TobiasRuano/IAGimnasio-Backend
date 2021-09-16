08eb8a408133b9dc05f942f8d4bbd303505502dd
Tobias Ruano <ruano.t10@gmail.com>
2021-09-16 16:55:25 -0300

    Fix nombre variables

--- queries/getAllAppointments.query.sql
+++ queries/getAllAppointments.query.sql
@@ -1,3 +1,3 @@
-select Appointments.id, Appointments.description, Appointments.name, Appointments.schedule, Appointments.trainnerID, Employees.name, Employees.surname from Appointments
+select Appointments.id, Appointments.description, Appointments.name, Appointments.schedule, Appointments.trainnerID, Employees.name as trainnerName, Employees.surname as trainnerSurname from Appointments
 	join Employees
 		on Appointments.trainnerID = Employees.id
