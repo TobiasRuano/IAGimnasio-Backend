a08ef000a443208a8af07ec49e9dcfabf9204aaf
Tobias Ruano <ruano.t10@gmail.com>
2021-09-22 19:09:04 -0300

    Update getAllUsers.query.sql

--- queries/getAllUsers.query.sql
+++ queries/getAllUsers.query.sql
@@ -1,4 +1,4 @@
-select Users.id, Users.name, Users.surname, Users.dni, Users.email, Users.birthday, Users.address, Users.phone, a.subscriptionType as type, HealthRecords.medicalDischarge, HealthRecords.data as healthData from Users 
+select Users.id, Users.name, Users.surname, Users.dni, Users.email, Users.birthday, Users.address, Users.phone, a.subscriptionType as type, HealthRecords.medicalDischarge, HealthRecords.data as data from Users 
 	left join (select * from UserSubscriptions where UserSubscriptions.endDate > :today) as a
 		on Users.id = a.userID
     left join HealthRecords 
